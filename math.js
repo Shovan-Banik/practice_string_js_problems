cade505dfe3bf271cbce26d3a2a7e1698a72cb1d
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-02 04:28:18 +0600

    math function

---
+++ math.js
@@ -0,0 +1,54 @@
+// //math.pow
+// var number=Math.pow(2,5);
+// console.log(number);
+
+
+
+// // math.abs
+// var num1=32;
+// var num2=52;
+// var gap=num1-num2;
+// if(gap<5){         // aikhane - value asbe tai gap besi hoar por..o first loop a dhokbe
+//     console.log('you are active');
+// }
+// else{
+//     console.log('you to geya');
+// }
+
+// var num1=32;
+// var num2=52;
+// var gap=Math.abs(num1-num2);
+// if(gap<5){         // math.abs dile aita - ta k na count kre sudhu value ta count krbe
+//                     // fole second loop a dhokbe
+//     console.log('you are active');
+// }
+// else{
+//     console.log('you to geya');
+// }
+
+
+// // Math.round
+// var i=234.4849284; //round korle 2.5 ar niche thakle 2 dekhabe
+// var j=2.50009;        // 2.5 ar opor thakle 3 dekhabe
+// console.log(Math.round(i));
+// console.log(Math.round(j));
+
+
+// // Math.ceil  and math.floor
+// console.log(Math.ceil(i));
+// console.log(Math.floor(j));
+
+
+// Math.random
+
+// var s=Math.round(Math.random()*100);
+//     console.log(s);
+
+for(var i=0; i<6;i++){
+    var s=Math.round(Math.random()*6);
+    console.log(s);
+}
+
+
+
+
