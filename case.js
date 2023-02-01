c377601acc61358e025f4a9ec112de2bab549a9c
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-02 02:47:17 +0600

    case conversion

---
+++ case.js
@@ -0,0 +1,8 @@
+var user='Akbar Ali'
+var admin='Akbar ali'
+if(user.toLowerCase()===admin.toLowerCase()){   //2 ta user k lower kore neyai valid hocce...nahole invalid dekhato
+    console.log("valid user");
+}
+else{
+    console.log("invalid user");
+}
