03bc265dba2a5eca566e5b15953b9566d4adb6e4
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-02 03:58:32 +0600

    code with cmnt

---
+++ split_trim.js
@@ -0,0 +1,22 @@
+// const gan="ami tomake bole debo, ki j aka dirgho rat, ami hete geci biran pothe";
+// var broken=gan.split('');
+// var broken1=gan.split(' ');
+// var broken2=gan.split(',');
+// console.log(broken);
+// console.log(broken1);
+// console.log(broken2);
+
+
+// slice ()
+// var tokra=gan.slice(4,8);
+// console.log(tokra);
+
+
+// substring ()
+
+// console.log(gan.substring(4,8));   [slice and substring are same as per result they given]
+
+
+// join
+var arr=['saff kre de moila','doa kre de','pagol koira de'];
+console.log(arr.join('. '));
